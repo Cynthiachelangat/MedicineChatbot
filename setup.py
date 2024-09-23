38a0383394f340a3f967bf8c9ef0d4ee8d89b203
Cynthiachelangat <cchela789@gmail.com>
2024-09-23 12:28:36 +0300

    Business Understanding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ='MedicalChatbot',
+    version= '0.0.0',
+    author= 'Cynthia Chelangat',
+    author_email= 'cchela789@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
