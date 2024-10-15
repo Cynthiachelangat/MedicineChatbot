164ccc3f19e715e832dea4b78dca5954ffbe802c
Cynthiachelangat <cchela789@gmail.com>
2024-10-15 14:53:30 +0300

    chat

--- app.py
+++ app.py
@@ -30,3 +30,32 @@ llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
                   model_type="llama",
                   config={'max_new_tokens':512,
                           'temperature':0.8})
+
+qa=RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True, 
+    chain_type_kwargs=chain_type_kwargs)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
